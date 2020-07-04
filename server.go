edf44e785c53976581535f3b5a94d8ce39382db6
lukechampine <luke.champine@gmail.com>
2020-07-04 00:16:28 -0400

    don't default to HTTPS in shard proxy

--- server.go
+++ server.go
@@ -371,9 +371,6 @@ func NewServer(dir string, wallet proto.Wallet, tpool proto.TransactionPool, sha
 	mux.HandleFunc("/scan", srv.handleScan)
 
 	// shard proxy
-	if !strings.HasPrefix(shardAddr, "https://") && !strings.HasPrefix(shardAddr, "http://") {
-		shardAddr = "https://" + shardAddr
-	}
 	shardURL, err := url.Parse(shardAddr)
 	if err != nil {
 		return nil, err
